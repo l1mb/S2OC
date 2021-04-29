31bd4f44d6a6aeb93d5eb837b80f597c17d86aa3
Сергей Сергеев <sergeev.mark@outlook.com>
2021-04-29 14:12:24 +0300

    Add auth window

---
+++ HabbitCracker/HabbitCracker/ViewModel/MainViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Security.AccessControl;
+using System.Threading.Channels;
+using System.Windows.Controls;
+using static System.Configuration.ConfigurationManager;
+
+namespace HabbitCracker.ViewModel
+{
+    internal class MainViewModel : BaseViewModel
+    {
+        private Page _databasePage;
+        private Page _profilePage;
+
+        private Page _currentPage;
+
+        public Page CurrentPage
+        {
+            get => _currentPage;
+            set
+            {
+                _currentPage = value;
+                OnPropertyChanged(nameof(CurrentPage));
+            }
+        }
+
+        public RelayCommand SetDatabasePageCommand => new RelayCommand(obj =>
+            {
+                CurrentPage = _databasePage;
+            }
+        );
+
+        public MainViewModel()
+        {
+            _databasePage = new View.DataBase();
+            _profilePage = new View.Profile();
+            CurrentPage = _profilePage;
+        }
+    }
+}
